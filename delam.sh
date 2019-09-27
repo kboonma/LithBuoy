c78f5e59cb15b7b0647eaf23ce217f321276697e
Kittiphon Boonma <kittiphon.b@gmail.com>
2019-09-27 05:32:06 +0200

    Create delam.sh

---
+++ delam.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# for i in `seq 0 1 5`; do python3 new_march_meeting.py 7 4 $i; done
+# for i in `seq 6 1 10`; do python3 new_march_meeting.py 7 4 $i; done
+# for i in `seq 11 1 15`; do python3 new_march_meeting.py 7 4 $i; done
+# for i in `seq 16 1 20`; do python3 new_march_meeting.py 7 4 $i; done
+# for i in `seq 0 1 5`; do python3 new_march_meeting.py 8 4 $i; done
+# for i in `seq 6 1 10`; do python3 new_march_meeting.py 8 4 $i; done
+# for i in `seq 11 1 15`; do python3 new_march_meeting.py 8 4 $i; done
+# for i in `seq 16 1 20`; do python3 new_march_meeting.py 8 4 $i; done
+
+# for i in `seq 0 1 10`; do python3 new_march_meeting.py 7 20 $i; done
+# for i in `seq 11 1 20`; do python3 new_march_meeting.py 7 20 $i; done
+# for i in `seq 0 1 10`; do python3 new_march_meeting.py 7 80 $i; done
+# for i in `seq 11 1 20`; do python3 new_march_meeting.py 7 80 $i; done
+# for i in `seq 0 1 10`; do python3 new_march_meeting.py 8 20 $i; done
+# for i in `seq 11 1 20`; do python3 new_march_meeting.py 8 20 $i; done
+# for i in `seq 0 1 10`; do python3 new_march_meeting.py 8 80 $i; done
+# for i in `seq 11 1 20`; do python3 new_march_meeting.py 8 80 $i; done
+
+# python3 new_march_meeting.py 5 4 33
